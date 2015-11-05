a3428aea4f07f1b98fbc1e302c104fd2247c36d4
Deepak Unni <deepak.unni3@gmail.com>
2015-11-05 07:07:16 -0600

    Improvements to ensembl-gff source
    Former-commit-id: 164353d92ccb00efec87572ac19d7c248f6e600c

--- bio/sources/ensembl-gff/main/src/org/intermine/bio/dataconversion/EnsemblGffGFF3RecordHandler.java
+++ bio/sources/ensembl-gff/main/src/org/intermine/bio/dataconversion/EnsemblGffGFF3RecordHandler.java
@@ -1,7 +1,7 @@
 package org.intermine.bio.dataconversion;
 
 /*
- * Copyright (C) 2002-2011 FlyMine
+ * Copyright (C) 2002-2015 FlyMine
  *
  * This code may be freely distributed and modified under the
  * terms of the GNU Lesser General Public Licence.  This should
@@ -10,24 +10,23 @@ package org.intermine.bio.dataconversion;
  *
  */
 
+import java.lang.System;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.net.URLDecoder;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import org.intermine.bio.io.gff3.GFF3Record;
 import org.intermine.metadata.Model;
 import org.intermine.metadata.StringUtil;
 import org.intermine.xml.full.Item;
-
-import java.util.HashMap;
-import java.util.HashSet;
-import java.net.URLDecoder;
 import java.util.Map;
 import java.util.Map.Entry;
-
 
 /**
  * A converter/retriever for the EnsemblGff dataset via GFF files.
@@ -35,17 +34,20 @@ import java.util.Map.Entry;
 
 public class EnsemblGffGFF3RecordHandler extends GFF3RecordHandler
 {
-     Map<String,String> aliasToRefId = new HashMap<String,String>();
-    Map<String,String> geneToRefId = new HashMap<String,String>();
     /**
      * Create a new EnsemblGffGFF3RecordHandler for the given data model.
      * @param model the model for which items will be created
      */
+
+    Map<String,String> aliasToRefId = new HashMap<String,String>();
+    Map<String,String> geneToRefId = new HashMap<String,String>();
+    Map<String,String> xRefToRefId = new HashMap<String,String>();
+
     public EnsemblGffGFF3RecordHandler (Model model) {
         super(model);
         refsAndCollections.put("Transcript", "gene");
         refsAndCollections.put("MRNA", "gene");
-    	refsAndCollections.put("TRNA", "gene");
+        refsAndCollections.put("TRNA", "gene");
         refsAndCollections.put("MiRNA", "gene");
         refsAndCollections.put("RRNA", "gene");
         refsAndCollections.put("SnRNA", "gene");
@@ -72,50 +74,24 @@ public class EnsemblGffGFF3RecordHandler extends GFF3RecordHandler
                 String id = record.getAttributes().get("ID").iterator().next();
                 feature.setAttribute("primaryIdentifier", id);
             }
-            if(record.getAttributes().get("source") != null){
-                String source = record.getAttributes().get("source").iterator().next();
-                //feature.setAttribute("source", source);
-            }
-
             if(record.getAttributes().get("symbol_ensembl") != null){
                 String symbol = record.getAttributes().get("symbol_ensembl").iterator().next();
                 feature.setAttribute("symbol", symbol);
             }
 
-            List<String> aliases = record.getAliases();  // getAliases() is a method predefined in Intermine
-            if (aliases != null) {
+            if (record.getAliases() != null) {
+                List<String> aliases = record.getAliases();
                 Iterator<String> aliasesIterator = aliases.iterator();
-                while(aliasesIterator.hasNext()) {
-                    // iterating through the list of aliases
-                    String ref = aliasesIterator.next();
-                    String[] splitVal = ref.split(" ");
-                    String ssource = splitVal[1];
-                    String aliasPrimaryIdentifier = splitVal[0];
-                    if (aliasToRefId.containsKey(aliasPrimaryIdentifier)) {
-                        feature.addToCollection("alias", aliasToRefId.get(aliasPrimaryIdentifier));
-                        if (! geneToRefId.containsKey(aliasPrimaryIdentifier)) {
-                            Item geneItem = converter.createItem("Gene");
-                            geneItem.setAttribute("primaryIdentifier", aliasPrimaryIdentifier);
-                            geneToRefId.put(aliasPrimaryIdentifier, geneItem.getIdentifier());
-                            addItem(geneItem);
-                        }
-                    } else {
-                        Item aliasItem = converter.createItem("AliasName");  // creating an AliasName object
-                        aliasItem.setAttribute("source", splitVal[1]);
-                        aliasItem.setAttribute("primaryIdentifier", splitVal[0]);  // setting primaryIdentifier of AliasName object
-                        String aliasRefId = aliasItem.getIdentifier();  // getting the reference ID of the AliasName object (needed for linking AliasName object to Gene object)
-                        feature.addToCollection("alias", aliasRefId);  // addToCollection creates the link between feature (Gene) and the AliasName object
-                        aliasItem.addToCollection("gene", feature.getIdentifier());  // and vice-versa
-                        aliasToRefId.put(aliasPrimaryIdentifier, aliasRefId);
-                        if (! geneToRefId.containsKey(aliasPrimaryIdentifier)) {
-                            Item geneItem = converter.createItem("Gene");
-                            geneItem.setAttribute("primaryIdentifier", aliasPrimaryIdentifier);
-                            geneToRefId.put(aliasPrimaryIdentifier, geneItem.getIdentifier());
-                            aliasItem.addToCollection("gene", geneItem.getIdentifier());
-                            addItem(geneItem);
-                        }
-                        addItem(aliasItem);  // adding AliasName object to be loaded into the database
-                    }
+                while (aliasesIterator.hasNext()) {
+                    setAliasName(aliasesIterator.next());
+                }
+            }
+
+            if (record.getAttributes().get("xRef") != null) {
+                List<String> xRefList = record.getAttributes().get("xRef");
+                Iterator<String> xRefIterator = xRefList.iterator();
+                while (xRefIterator.hasNext()) {
+                    setCrossReference(xRefIterator.next());
                 }
             }
         }
@@ -123,10 +99,6 @@ public class EnsemblGffGFF3RecordHandler extends GFF3RecordHandler
             if(record.getAttributes().get("ID") != null){
                 String id = record.getAttributes().get("ID").iterator().next();
                 feature.setAttribute("primaryIdentifier", id);
-            }
-            if(record.getAttributes().get("source") != null){
-                String source = record.getAttributes().get("source").iterator().next();
-                //feature.setAttribute("source", source);
             }
             if(record.getAttributes().get("symbol_ensembl") != null){
                 String symbol = record.getAttributes().get("symbol_ensembl").iterator().next();
@@ -141,9 +113,84 @@ public class EnsemblGffGFF3RecordHandler extends GFF3RecordHandler
             // Do nothing
         }
         else {
-            System.out.println(clsName);
-            System.exit(0);
+            System.out.println("Unaccounted class type encountered: " + clsName);
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Method parses the alias string, creates an AliasName item and sets the necessary references and collections
+     * @param alias
+     */
+    public void setAliasName(String alias) {
+        Item feature = getFeature();
+        List<String> splitVal = new ArrayList<String>(Arrays.asList(StringUtil.split(alias, ":")));
+        if (splitVal.size() != 2) {
+            System.out.println("Ambiguous aliasName: " + splitVal);
+            System.out.println("Expected aliasName format is '<ALIAS_ID> <ALIAS_SOURCE>'");
+            System.out.println("Note: ALIAS_ID must be associated with its source");
+            System.exit(1);
+        }
+        String aliasPrimaryIdentifier = splitVal.get(0);
+        String aliasSource = splitVal.get(1);
+        if (aliasToRefId.containsKey(aliasPrimaryIdentifier)) {
+            feature.addToCollection("alias", aliasToRefId.get(aliasPrimaryIdentifier));
+        } else {
+            Item aliasItem = converter.createItem("AliasName");  // create an AliasName object
+            aliasItem.setAttribute("identifier", aliasPrimaryIdentifier);  // set identifier of AliasName object
+            aliasItem.setAttribute("source", aliasSource);
+            aliasItem.setReference("organism", getOrganism());
+            String aliasRefId = aliasItem.getIdentifier();  // get the reference ID of the AliasName object (needed for linking AliasName object to Gene object)
+            feature.addToCollection("alias", aliasRefId);  // addToCollection creates the link between feature (Gene) and the AliasName object
+            aliasItem.addToCollection("gene", feature.getIdentifier());  // and vice-versa
+            aliasToRefId.put(aliasPrimaryIdentifier, aliasRefId);
+            addItem(aliasItem);  // add AliasName object to be loaded into the database
+        }
+    }
+
+    /**
+     * Method parses the xRef string, creates a xRef item, creates a Gene item and sets the necessary references and collections
+     * @param xRef
+     */
+    public void setCrossReference(String xRef) {
+        Item feature = getFeature();
+        List<String> xRefPair = new ArrayList<String>(Arrays.asList(StringUtil.split(xRef, ":")));
+        if (xRefPair.size() == 0) { return; }
+        if (xRefPair.size() != 2) {
+            System.out.println("Ambiguous xRef: " + xRefPair);
+            System.out.println("Expected xRef format is '<XREF_ID> <XREF_SOURCE>'");
+            System.out.println("Note: XREF_SOURCE should match column 2 of the alternate GFF3 (if any)");
+            System.exit(1);
         }
 
+        String identifier = xRefPair.get(0);
+        String xRefSource = xRefPair.get(1);
+        if (xRefToRefId.containsKey(identifier)) {
+            feature.setReference("crossReference", xRefToRefId.get(identifier));
+            if (! geneToRefId.containsKey(identifier)) {
+                System.out.println("xRef exists but its corresponding gene instance does not exist");
+                System.exit(1);
+            }
+        } else {
+            Item xRefItem = converter.createItem("xRef");  // creating an xRef object
+            xRefItem.setAttribute("identifier", identifier);  // setting primaryIdentifier of xRef object
+            xRefItem.setAttribute("source", xRefSource);
+            xRefItem.setReference("organism", getOrganism());
+            String xRefRefId = xRefItem.getIdentifier();  // getting the reference ID of the xRef object (needed for linking AliasName object to Gene object)
+            feature.setReference("crossReference", xRefRefId);  // setReference creates the link between feature (Gene) and the AliasName object
+            // xRefItem.addToCollection("geneCrossReference", feature.getIdentifier());  // and vice-versa
+            xRefToRefId.put(identifier, xRefRefId);
+            if (!geneToRefId.containsKey(identifier)) {
+                // storing the Gene instance of xRef
+                Item geneItem = converter.createItem("Gene");
+                geneItem.setAttribute("primaryIdentifier", identifier);
+                geneItem.setAttribute("source", xRefSource);
+                geneItem.setReference("organism", getOrganism());
+                geneToRefId.put(identifier, geneItem.getIdentifier());
+                xRefItem.setReference("gene", geneItem.getIdentifier());
+                addItem(geneItem);
+            }
+            addItem(xRefItem);
+        }
     }
 }
