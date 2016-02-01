f392f7d6d3f4a395e0e10595b17756cb2d7b2808
aditi <tayala@missouri.edu>
2016-02-01 14:38:26 -0600

    Hapmap ss_id alias
    Former-commit-id: 6b15749ddd0d0562ade4acda128c0971b3e773ef [formerly 96eb5f9934d303ee9fc36d09678a7fe76938e100]
    Former-commit-id: d968f4c6b5cfa406487eda3a5cfa5deeccc8dc37

--- bio/sources/hapmap-gff/main/src/org/intermine/bio/dataconversion/HapmapGffGFF3RecordHandler.java
+++ bio/sources/hapmap-gff/main/src/org/intermine/bio/dataconversion/HapmapGffGFF3RecordHandler.java
@@ -12,22 +12,46 @@ package org.intermine.bio.dataconversion;
 
 import org.intermine.bio.io.gff3.GFF3Record;
 import org.intermine.metadata.Model;
-import org.intermine.xml.full.Item;
 import org.intermine.metadata.StringUtil;
+import org.intermine.xml.full.Item;
+
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
+import java.util.Iterator;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.intermine.bio.util.OrganismRepository;
+import org.intermine.dataconversion.ItemWriter;
+import org.intermine.objectstore.ObjectStoreException;
+import org.intermine.util.SAXParser;
+import org.intermine.xml.full.ReferenceList;
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
 /**
- * A converter/retriever for the HapmapGff dataset via GFF files.
+ * A converter/retriever for Hapmap GFF3
  */
 
 public class HapmapGffGFF3RecordHandler extends GFF3RecordHandler
 {
+ Map<String,String> aliasToRefId = new HashMap<String,String>();
 
     /**
-     * Create a new HapmapGffGFF3RecordHandler for the given data model.
+     * Create a new HapmapGFF3RecordHandler for the given data model.
      * @param model the model for which items will be created
      */
     public HapmapGffGFF3RecordHandler (Model model) {
@@ -46,7 +70,7 @@ public class HapmapGffGFF3RecordHandler extends GFF3RecordHandler
         if (clsName.equals("SNP")) {
             if (record.getAttributes().get("ID") != null) {
                 String secondaryIdentifier = record.getAttributes().get("ID").iterator().next();
-                feature.setAttribute("primaryIdentifier", secondaryIdentifier);
+                feature.setAttribute("secondaryIdentifier", secondaryIdentifier);
             }
             if (record.getAttributes().get("Reference_seq") != null) {
                 String refAllele = record.getAttributes().get("Reference_seq").iterator().next();
@@ -70,11 +94,59 @@ public class HapmapGffGFF3RecordHandler extends GFF3RecordHandler
                             throw new RuntimeException("Error in Dbxref attribute " + ref);
                         }
                         if (ref.startsWith("dbSNP")) {
-                            feature.setAttribute("secondaryIdentifier", ref.replace("dbSNP:", ""));
+                            feature.setAttribute("primaryIdentifier", ref.replace("dbSNP:", ""));
                         }
                     }
                 }
             }
-        }
+
+          if (record.getAliases() != null) {
+                List<String> aliases = record.getAliases();
+                Iterator<String> aliasesIterator = aliases.iterator();
+                while (aliasesIterator.hasNext()) {
+                    setAliasName(aliasesIterator.next());
+                }
+            }
+        
+
+
+       }
+
     }
+
+
+       
+     public void setAliasName(String alias) {
+        // TODO: Should the relationship between Gene and AliasName be a Collection or a Reference?
+        Item feature = getFeature();
+        List<String> splitVal = new ArrayList<String>(Arrays.asList(StringUtil.split(alias, ":")));
+        if (splitVal.size() != 2) {
+            System.out.println("size: " + splitVal.size());
+            System.out.println("Ambiguous aliasName: " + splitVal);
+            System.out.println("Expected aliasName format is '<ALIAS_ID>:<ALIAS_SOURCE>'");
+            System.out.println("Note: ALIAS_ID must be associated with its source");
+            System.exit(1);
+        }
+        String aliasPrimaryIdentifier = splitVal.get(0);
+        String aliasSource = splitVal.get(1);
+            Item aliasItem = converter.createItem("AliasName");
+            aliasItem.setAttribute("identifier", aliasPrimaryIdentifier);
+            aliasItem.setAttribute("source", aliasSource);
+            aliasItem.setReference("organism", getOrganism());
+            String aliasRefId = aliasItem.getIdentifier();
+            feature.addToCollection("alias", aliasRefId);
+            aliasItem.addToCollection("gene", feature.getIdentifier());
+            aliasToRefId.put(aliasPrimaryIdentifier, aliasRefId);
+            addItem(aliasItem);
+        
+    }
+
+
+
+
+
 }
+
+
+
+
