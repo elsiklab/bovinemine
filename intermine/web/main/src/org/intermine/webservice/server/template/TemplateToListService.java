2561d8188c43a70064cba35d0a5930faa7bbc659
Deepak Unni <deepak.unni3@gmail.com>
2016-03-01 09:51:06 -0600

    Remove artefacts of git merge
    Former-commit-id: 198211dd9229fc11e56310d5f270a890e88992a9

--- intermine/web/main/src/org/intermine/webservice/server/template/TemplateToListService.java
+++ intermine/web/main/src/org/intermine/webservice/server/template/TemplateToListService.java
@@ -111,19 +111,8 @@ public class TemplateToListService extends QueryToListService
         }
 
         PathQuery pq = populatedTemplate.getQueryToExecute();
-<<<<<<< HEAD
-
-        List<String> oldView = pq.getView();
-        oldView.remove(newViewString);
         pq.clearView();
         pq.addView(newViewString);
-        // Make sure these are added back to keep the query structure the same
-        pq.addViews(oldView);
-
-=======
-        pq.clearView();
-        pq.addView(newViewString);
->>>>>>> intermine-1.6.5
         return pq;
     }
 }
